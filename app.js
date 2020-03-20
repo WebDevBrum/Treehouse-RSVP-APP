f4dd119f7d3d8e459a8501f34390973f12d8bd68
Scott Davies <57508310+theDevelopingDeveloper@users.noreply.github.com>
2020-03-20 06:11:24 +0000

    added function to createLI and remove functionality

--- app.js
+++ app.js
@@ -2,18 +2,32 @@ const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 
+function createLI(text) { //function created as submit lister got busy, note use of 'text' made transition easier to manage
+  const li = document.createElement('li');
+  li.textContent = text;
+  
+  const label = document.createElement('label');
+  label.textContent = 'confirmed';
+  
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  
+  label.appendChild(checkbox);
+  li.appendChild(label);
+
+  const button = document.createElement('button');
+  button.textContent = 'remove';
+  li.appendChild(button);
+
+  return li;
+}
+
+
 form.addEventListener('submit', (e) => {
   e.preventDefault(); //cancels default behaviour eg page refresh on submit
   const text = input.value;
   input.value = '';
-  const li = document.createElement('li');
-  li.textContent = text;
-  const label = document.createElement('label');
-  label.textContent = 'confirmed';
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  label.appendChild(checkbox);
-  li.appendChild(label);
+  const li = createLI(text);
   ul.appendChild(li);
 });
 
@@ -26,6 +40,13 @@ ul.addEventListener('change', (e) => {
         listItem.className = 'responded';
     } else {
         listItem.className = '';
-
     }
+});
+
+ul.addEventListener('click', (e) => {
+   if (e.target.tagName === 'BUTTON') {
+      const li = e.target.parentNode; 
+      const ul = li.parentNode;
+      ul.removeChild(li);
+   } 
 });
